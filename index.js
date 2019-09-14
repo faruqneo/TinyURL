9390162511fb165a2c110ef0b07a070ac3fef8c8
faruqneo <faruq@marmeto.com>
2019-09-14 14:33:08 +0530

    shortURL convert

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
